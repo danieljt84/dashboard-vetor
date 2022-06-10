d07dd927e553c98b75af4a2eb5b387d1d0ca30ca
Daniel <danieljt84@gmail.com>
2022-06-10 09:59:58 -0300

    Criacao do grafico de verba utilizada

--- src/app/components/main/chart-verba/chart-verba.component.ts
+++ src/app/components/main/chart-verba/chart-verba.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataset,ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-chart-verba',
@@ -6,7 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chart-verba.component.css']
 })
 export class ChartVerbaComponent implements OnInit {
+  public chartLabels = ['semana 1', 'semana 2', 'semana 3', 'semana 4'];
+  public barChartOptions: ChartOptions = {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
 
+  public barChartData: ChartDataset[] = [
+    {
+      barThickness: 35,
+      backgroundColor: '#2E59A7',
+      type: 'bar',
+      label: 'Bar Dataset',
+      data: [20000, 32000, 18000, 40000],
+      indexAxis: 'x',
+      order: 2
+    }, {
+      type: 'line',
+      label: 'Line Dataset',
+      data: [18000, 35000, 20000, 30000],
+      order:1
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
